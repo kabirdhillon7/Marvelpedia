23b22cd8e25e9a3be83f7fe152184ea4604300b0
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-09-05 21:40:22 -0700

    Initial Commit

---
+++ Marvelpedia/MarvelpediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelpediaApp.swift
+//  Marvelpedia
+//
+//  Created by Kabir Dhillon on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelpediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
